77cf5725c193a4340f1680ad43f3f98d54a02ac8
Dvoriadkin, Nikita <dvorinik@fel.cvut.cz>
2021-04-16 19:30:52 +0200

    Generate PIN if was absent before

--- src/constants/rest_constants.js
+++ src/constants/rest_constants.js
@@ -1,1 +1,5 @@
-export const LOGIN_URI = 'http://polar-hamlet-51612.herokuapp.com/auth/login';
+const BASE_URL = 'http://polar-hamlet-51612.herokuapp.com';
+
+export const LOGIN_URI = `${BASE_URL}/auth/login`;
+export const GET_PIN_URI = `${BASE_URL}/auth/pin`;
+export const GENERATE_PIN_URI = `${BASE_URL}/auth/generatePin`;
