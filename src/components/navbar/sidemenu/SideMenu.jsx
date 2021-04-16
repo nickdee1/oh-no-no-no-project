828e7f704c9287797b1581690493be3782fd5dde
Dvoriadkin, Nikita <dvorinik@fel.cvut.cz>
2021-04-17 01:31:35 +0200

    Login error validation, logout, PIN in menu

--- src/components/navbar/sidemenu/SideMenu.jsx
+++ src/components/navbar/sidemenu/SideMenu.jsx
@@ -9,10 +9,13 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import IconButton from '@material-ui/core/IconButton';
 import { NavLink } from 'react-router-dom';
-import { logOut } from '../../../pages/login/loginSlice';
-import { useAppDispatch } from '../../../hooks';
+import { useSnackbar } from 'notistack';
+import { logOut, setRole, setToken } from '../../../pages/login/loginSlice';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { userService } from '../../../services/login';
 
 const useStyles = makeStyles({
   list: {
@@ -27,6 +30,9 @@ export default function SideMenu() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const dispatch = useAppDispatch();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const token = useAppSelector((state) => state.login.token);
+  const pin = useAppSelector((state) => state.login.pin);
 
   const toggleDrawer = (value) => (event) => {
     if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -36,6 +42,16 @@ export default function SideMenu() {
     setOpen(value);
   };
 
+  function performLogout() {
+    userService.logout(token)
+      .then(() => {
+        dispatch(logOut());
+        dispatch(setRole(''));
+        dispatch(setToken(''));
+        enqueueSnackbar('You were successfully logged out', { variant: 'success' });
+      });
+  }
+
   const list = (
     <div
       className={clsx(classes.list, false)}
@@ -44,6 +60,11 @@ export default function SideMenu() {
       onKeyDown={toggleDrawer(false)}
     >
       <List>
+        <ListItem key="pin">
+          <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+          <ListItemText primary={ 'PIN: ' + pin } />
+        </ListItem>
+        <Divider />
         <NavLink to="/plan">
           <ListItem button key="plan">
             <ListItemIcon><MailIcon /></ListItemIcon>
@@ -65,7 +86,7 @@ export default function SideMenu() {
       </List>
       <Divider />
       <List>
-        <ListItem button key="plan" onClick={() => dispatch(logOut())}>
+        <ListItem button key="plan" onClick={() => performLogout()}>
           <ListItemIcon><MailIcon /></ListItemIcon>
           <ListItemText primary="Logout" />
         </ListItem>
--- src/services/login.js
+++ src/services/login.js
@@ -1,54 +1,71 @@
-import { LOGIN_URI, GET_PIN_URI, GENERATE_PIN_URI } from '../constants/rest_constants';
+import { LOGIN_URI, LOGOUT_URI, GET_PIN_URI, GENERATE_PIN_URI } from '../constants/rest_constants';
 
 const getTokenOperations = (token) => ({
   method: 'GET',
   headers: { 'Content-Type': 'application/json', 'X-authSessionId': token },
 });
 
-const generateTokenOperations = (token) => ({
+const postTokenOperations = (token) => ({
   method: 'POST',
   headers: { 'Content-Type': 'application/json', 'X-authSessionId': token },
 });
 
-function handleResponse(response) {
+function handleJSONResponse(response) {
   return response.text().then((text) => {
     const data = text && JSON.parse(text);
     if (!response.ok) {
-      const error = (data && data.message) || response.statusText;
+      const error = response.status;
       return Promise.reject(error);
     }
     return data;
   });
 }
 
-function login(username, password) {
+function handleSimpleResponse(response) {
+  return response.text().then((text) => {
+    const data = text;
+    if (!response.ok) {
+      const error = response.status;
+      return Promise.reject(error);
+    }
+    return data;
+  });
+}
+
+function login(email, password) {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ email, password }),
   };
 
   return fetch(LOGIN_URI, requestOptions)
-    .then(handleResponse)
-    .then((result) => result.authSessionId);
+    .then(handleJSONResponse)
+    .then((result) => result);
+}
+
+function logout(token) {
+  const requestOptions = postTokenOperations(token);
+  return fetch(LOGOUT_URI, requestOptions).then(handleSimpleResponse).then(result => result)
 }
 
 function firstCheckPin(token) {
   const requestOptions = getTokenOperations(token);
   return fetch(GET_PIN_URI, requestOptions)
-    .then(handleResponse)
-    .then((result) => result.pin)
+    .then(handleJSONResponse)
+    .then((result) => result.pin);
 }
 
 function getGeneratedPin(token) {
-  const requestOptions = generateTokenOperations(token);
+  const requestOptions = postTokenOperations(token);
   return fetch(GENERATE_PIN_URI, requestOptions)
-    .then(handleResponse)
+    .then(handleJSONResponse)
     .then((result) => result.pin);
 }
 
 export const userService = {
   login,
+  logout,
   firstCheckPin,
   getGeneratedPin,
 };
