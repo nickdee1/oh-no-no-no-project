c403081a0654891afdd230666034bf5348c141b3
Dvoriadkin, Nikita <dvorinik@fel.cvut.cz>
2021-04-17 14:24:01 +0200

    beautify

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -3,14 +3,22 @@ import { Grid, Typography } from '@material-ui/core';
 import NavigationBar from '../../components/navbar/NavigationBar';
 import HistoryCard from "../../components/HistoryCard";
 import ReservationCard from "./ReservationCard";
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: 20
+  }
+});
 
 function Main() {
+  const classes = useStyles();
   return (
     <Grid container direction="column">
       <Grid item>
         <NavigationBar title="Main" />
       </Grid>
-      <Grid item container>
+      <Grid className={classes.root} item container>
         <Grid item xs={1} md={2} />
         <Grid item xs={10} md={8}>
             <ReservationCard/>
